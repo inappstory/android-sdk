054a2cb414483b0c7a33ddff263783ec565e000f
paperrose <paperrose@mail.ru>
2022-02-07 19:17:16 +0300

    share api 22

--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/utils/StoryShareBroadcastReceiver.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/utils/StoryShareBroadcastReceiver.java
@@ -14,7 +14,10 @@ import static android.content.Intent.EXTRA_CHOSEN_COMPONENT;
 public class StoryShareBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        ComponentName clickedComponent = intent.getParcelableExtra(EXTRA_CHOSEN_COMPONENT);
+        ComponentName clickedComponent = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
+            clickedComponent = intent.getParcelableExtra(EXTRA_CHOSEN_COMPONENT);
+        }
         if (clickedComponent != null && ScreensManager.getInstance().getTempShareId() != null) {
             if (ScreensManager.getInstance().currentGameActivity != null) {
                 ScreensManager.getInstance().currentGameActivity.shareComplete(
