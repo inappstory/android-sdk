079a9f6c2daa2d738624bb2c7ba9f128a7f8aedc
paperrose <paperrose@mail.ru>
2023-02-07 14:27:14 +0300

    fix classcastexception

--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/ui/widgets/readerscreen/webview/SimpleStoriesWebView.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/ui/widgets/readerscreen/webview/SimpleStoriesWebView.java
@@ -12,6 +12,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewParent;
 import android.webkit.ConsoleMessage;
 import android.webkit.URLUtil;
 import android.webkit.WebChromeClient;
@@ -333,9 +334,17 @@ public class SimpleStoriesWebView extends IASWebView implements SimpleStoriesVie
         logMethod("goods_widget_complete " + widgetId);
     }
 
+    private boolean checkIfParentsHasCubeAnimation(ViewParent view) {
+        if (view == null) return false;
+        if (view instanceof ReaderPager) {
+            return ((ReaderPager) view).cubeAnimation;
+        }
+        return checkIfParentsHasCubeAnimation(view.getParentForAccessibility());
+    }
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent motionEvent) {
-        if (((ReaderPager) getParentForAccessibility()).cubeAnimation) return false;
+        if (checkIfParentsHasCubeAnimation(getParentForAccessibility())) return false;
         //if (!InAppStoryService.isConnected()) return true;
 
         switch (motionEvent.getActionMasked()) {
@@ -358,7 +367,8 @@ public class SimpleStoriesWebView extends IASWebView implements SimpleStoriesVie
 
     @Override
     public boolean onTouchEvent(MotionEvent motionEvent) {
-        if (((ReaderPager) getParentForAccessibility()).cubeAnimation) return false;
+
+        if (checkIfParentsHasCubeAnimation(getParentForAccessibility())) return false;
         boolean c = super.onTouchEvent(motionEvent);
         if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
             if (System.currentTimeMillis() - lastTap < 1500) {
@@ -374,7 +384,7 @@ public class SimpleStoriesWebView extends IASWebView implements SimpleStoriesVie
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent motionEvent) {
-        if (((ReaderPager) getParentForAccessibility()).cubeAnimation) return false;
+        if (checkIfParentsHasCubeAnimation(getParentForAccessibility())) return false;
         boolean c = super.onInterceptTouchEvent(motionEvent);
         if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
             if (System.currentTimeMillis() - lastTap < 1500) {
