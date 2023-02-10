1a4847a3933a97281893fd0564b065222b4f2863
paperrose <paperrose@mail.ru>
2023-02-10 16:14:27 +0300

    session messages for editor

--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/api/models/SessionEditor.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/api/models/SessionEditor.java
@@ -1,9 +1,11 @@
 package com.inappstory.sdk.stories.api.models;
 
 
+import java.util.HashMap;
+
 public class SessionEditor {
 
     public String url;
     public SessionEditorConfig config;
-
+    public HashMap<String, String> messages;
 }
--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/ui/list/StoryFavoriteListItem.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/ui/list/StoryFavoriteListItem.java
@@ -1,6 +1,5 @@
 package com.inappstory.sdk.stories.ui.list;
 
-import android.graphics.Color;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.LayoutInflater;
