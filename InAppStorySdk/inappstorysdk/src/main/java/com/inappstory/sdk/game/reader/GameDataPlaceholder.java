67428b4f1ee7c4637433f61d7c0d005a3e720e6b
paperrose <paperrose@mail.ru>
2023-03-20 12:45:43 +0300

    version game placeholder as public (obfuscation)

--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/game/reader/GameDataPlaceholder.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/game/reader/GameDataPlaceholder.java
@@ -1,14 +1,19 @@
 package com.inappstory.sdk.game.reader;
 
-class GameDataPlaceholder {
+import com.inappstory.sdk.network.SerializedName;
+
+public class GameDataPlaceholder {
     public GameDataPlaceholder(String type, String name, String value) {
         this.name = name;
         this.type = type;
         this.value = value;
     }
 
+    @SerializedName("name")
     public String name;
+    @SerializedName("type")
     public String type;
+    @SerializedName("value")
     public String value;
 
     public GameDataPlaceholder() {
