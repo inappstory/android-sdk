165df2f0afb7fc1734842add564df7fd9d6d751e
paperrose <paperrose@mail.ru>
2023-03-31 13:53:20 +0300

    fix game intent bugs

--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/game/reader/GameActivity.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/game/reader/GameActivity.java
@@ -667,16 +667,15 @@ public class GameActivity extends AppCompatActivity {
 
     void gameCompleted(String gameState, String link) {
         try {
-
-            int storyId;
+            String storyId;
             int slideIndex;
             String observableId;
             if (manager.storyId == null) {
-                storyId = Integer.parseInt(getIntent().getStringExtra("storyId"));
+                storyId = getIntent().getStringExtra("storyId");
                 slideIndex = getIntent().getIntExtra("slideIndex", 0);
                 observableId = getIntent().getStringExtra("observableId");
             } else {
-                storyId = Integer.parseInt(manager.storyId);
+                storyId = manager.storyId;
                 slideIndex = manager.index;
                 observableId = manager.observableId;
             }
@@ -689,16 +688,15 @@ public class GameActivity extends AppCompatActivity {
                     if (liveData != null) {
                         liveData.postValue(new GameCompleteEvent(
                                 gameState,
-                                storyId,
+                                Integer.parseInt(storyId),
                                 slideIndex));
                     }
                 }
-            } else {
-                intent.putExtra("storyId", storyId);
-                intent.putExtra("slideIndex", slideIndex);
-                if (gameState != null)
-                    intent.putExtra("gameState", gameState);
             }
+            intent.putExtra("storyId", storyId);
+            intent.putExtra("slideIndex", slideIndex);
+            if (gameState != null)
+                intent.putExtra("gameState", gameState);
             if (link != null)
                 manager.tapOnLink(link);
             setResult(RESULT_OK, intent);
