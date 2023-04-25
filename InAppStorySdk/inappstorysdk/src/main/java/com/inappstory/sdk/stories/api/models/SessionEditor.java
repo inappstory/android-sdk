718f7757affc73f1bd0858de948d51ea2ae658a8
paperrose <paperrose@mail.ru>
2023-04-25 07:38:38 +0300

    add UGC editor version map to Session model

--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/api/models/SessionEditor.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/api/models/SessionEditor.java
@@ -1,11 +1,21 @@
 package com.inappstory.sdk.stories.api.models;
 
 
+import com.inappstory.sdk.network.SerializedName;
+
 import java.util.HashMap;
+import java.util.List;
 
 public class SessionEditor {
 
+    @SerializedName("url")
     public String url;
+    @SerializedName("urlTemplate")
+    public String urlTemplate;
+    @SerializedName("versionTemplate")
+    public String versionTemplate;
+    @SerializedName("versionsMap")
+    public List<UGCVersionToSDKBuild> versionsMap;
     public HashMap<String, Object> config;
     public HashMap<String, String> messages;
 }
