a3b92b149731614f34ffbdace411c0c216c5b5e0
paperrose <paperrose@mail.ru>
2023-09-18 16:50:36 +0300

    fix game requests (quote)

--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/game/reader/GameManager.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/game/reader/GameManager.java
@@ -213,8 +213,7 @@ public class GameManager {
 
     private String modifyJsResult(String data) {
         if (data == null) return "";
-        data.replaceAll("'", "\\'");
-        return data;
+        return data.replaceAll("'", "\\\\'");
     }
 
     void tapOnLink(String link) {
