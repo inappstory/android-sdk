eb7ab176c1645857fea2faf1086b8c304a129a71
paperrose <paperrose@mail.ru>
2023-09-19 17:38:28 +0300

    add empty ListScrollCallbackAdapter

---
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/outercallbacks/storieslist/ListScrollCallbackAdapter.java
@@ -0,0 +1,38 @@
+package com.inappstory.sdk.stories.outercallbacks.storieslist;
+
+import com.inappstory.sdk.stories.ui.list.ShownStoriesListItem;
+
+import java.util.List;
+
+public abstract class ListScrollCallbackAdapter implements ListScrollCallback {
+
+    @Override
+    public void userInteractionStart() {
+
+    }
+
+    @Override
+    public void scrollStart() {
+
+    }
+
+    @Override
+    public void onOverscroll(int dx, int dy) {
+
+    }
+
+    @Override
+    public void onVisibleAreaUpdated(List<ShownStoriesListItem> shownStoriesListItemData) {
+
+    }
+
+    @Override
+    public void scrollEnd() {
+
+    }
+
+    @Override
+    public void userInteractionEnd() {
+
+    }
+}
