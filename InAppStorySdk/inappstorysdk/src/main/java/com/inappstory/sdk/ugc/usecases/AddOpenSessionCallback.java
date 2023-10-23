3ecc6f135276db99d12f75bc76e3b23e245496d2
paperrose <paperrose@mail.ru>
2023-10-23 15:20:47 +0300

    add session open static callbacks (for UGC SDK usage)

--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/ugc/usecases/AddOpenSessionCallback.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/ugc/usecases/AddOpenSessionCallback.java
@@ -4,7 +4,7 @@ import com.inappstory.sdk.stories.utils.SessionManager;
 import com.inappstory.sdk.ugc.extinterfaces.IOpenSessionCallback;
 
 public class AddOpenSessionCallback {
-    void add(IOpenSessionCallback callback) {
+    public void add(IOpenSessionCallback callback) {
         SessionManager.getInstance().addStaticOpenSessionCallback(callback);
     }
 }
