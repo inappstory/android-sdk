014fbfb952fd4df591b06de66e24603a50337db3
paperrose <paperrose@mail.ru>
2023-12-05 09:43:00 +0300

    adapter classes for IOpenGameReader and IOpenStoriesReader interfaces

---
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/outercallbacks/common/objects/IOpenGameReaderAdapter.java
@@ -0,0 +1,24 @@
+package com.inappstory.sdk.stories.outercallbacks.common.objects;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+public abstract class IOpenGameReaderAdapter implements IOpenGameReader {
+    public abstract void onOpen(
+            Context context,
+            Bundle bundle
+    );
+
+    public void onHideStatusBar(Context context) {
+    }
+
+    public void onRestoreStatusBar(Context context) {
+    }
+
+    public void onShowInFullscreen(Context context) {
+    }
+
+    public void onRestoreScreen(Context context) {
+    }
+}
---
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/outercallbacks/common/objects/IOpenStoriesReaderAdapter.java
@@ -0,0 +1,24 @@
+package com.inappstory.sdk.stories.outercallbacks.common.objects;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+public abstract class IOpenStoriesReaderAdapter implements IOpenStoriesReader {
+    public abstract void onOpen(
+            Context context,
+            Bundle bundle
+    );
+
+    public void onHideStatusBar(Context context) {
+    }
+
+    public void onRestoreStatusBar(Context context) {
+    }
+
+    public void onShowInFullscreen(Context context) {
+    }
+
+    public void onRestoreScreen(Context context) {
+    }
+}
