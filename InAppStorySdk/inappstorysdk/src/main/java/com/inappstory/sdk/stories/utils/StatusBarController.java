8d4068135695fe6958e4fbfe7250f69c9cce1c10
paperrose <paperrose@mail.ru>
2026-04-23 12:04:38 +0300

    fullscreen stories

--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/utils/StatusBarController.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/utils/StatusBarController.java
@@ -1,9 +1,12 @@
 package com.inappstory.sdk.stories.utils;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.inappstory.sdk.utils.SystemUiUtils;
 
 /**
  * Created by Paperrose on 23.07.2018.
@@ -14,14 +17,8 @@ public class StatusBarController {
 
     public static void showFullscreen(Activity context) {
         if (context == null) return;
-        context.getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        /*int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_FULLSCREEN |
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        decorView.setSystemUiVisibility(uiOptions);*/
+
+
     }
 
     public static void hideStatusBar(Activity context, boolean withSb) {
@@ -41,8 +38,6 @@ public class StatusBarController {
 
     public static void restore(Activity context) {
         if (context == null) return;
-        // Show Status Bar.
 
-        context.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
