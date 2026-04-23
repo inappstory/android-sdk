91a8385d967a825fa7774029355700d091a59864
paperrose <paperrose@mail.ru>
2026-04-23 11:18:19 +0300

    fullscreen stories

--- InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/utils/StatusBarController.java
+++ InAppStorySdk/inappstorysdk/src/main/java/com/inappstory/sdk/stories/utils/StatusBarController.java
@@ -2,6 +2,8 @@ package com.inappstory.sdk.stories.utils;
 
 import android.app.Activity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by Paperrose on 23.07.2018.
@@ -12,15 +14,14 @@ public class StatusBarController {
 
     public static void showFullscreen(Activity context) {
         if (context == null) return;
-        View decorView = context.getWindow().getDecorView();
-        // Hide Status Bar.
-        systemUiFlags = decorView.getSystemUiVisibility();
-        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+        context.getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        /*int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                 View.SYSTEM_UI_FLAG_FULLSCREEN |
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        decorView.setSystemUiVisibility(uiOptions);
+        decorView.setSystemUiVisibility(uiOptions);*/
     }
 
     public static void hideStatusBar(Activity context, boolean withSb) {
@@ -40,13 +41,8 @@ public class StatusBarController {
 
     public static void restore(Activity context) {
         if (context == null) return;
-        View decorView = context.getWindow().getDecorView();
         // Show Status Bar.
-        if (systemUiFlags != -1) {
-            decorView.setSystemUiVisibility(systemUiFlags);
-        } else {
-            int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
-            decorView.setSystemUiVisibility(uiOptions);
-        }
+
+        context.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
